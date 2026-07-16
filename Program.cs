f4c5c5b19ac575610ef8147ad88079bfd96f09b5
Philip2BautistaV <philip.ii.v.bautista@accenture.com>
2026-07-16 22:31:59 +0800

    Update deployment message in root endpoint

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Webapp deployment completed");
+app.MapGet("/", () => "Webapp deployment on Thursday");
 
 app.Run();
