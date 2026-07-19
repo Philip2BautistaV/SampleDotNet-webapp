ae9f2e9fa5d2c015feb54e12514f246880f5b3ee
Philip2BautistaV <philip.ii.v.bautista@accenture.com>
2026-07-19 16:54:36 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Webapp in main");
+app.MapGet("/", () => "Webapp in main on Sunday");
 
 app.Run();
